d6f73a27f399a0b37f5606ebf99c391c2e5a6ac9
Max <m.taubert2015@gmail.com>
2019-05-09 21:18:45 +0100

    new actions

--- Scripts/Prefab_Scripts/UI.gd
+++ Scripts/Prefab_Scripts/UI.gd
@@ -1,11 +1,12 @@
 extends Control
 
+#Setup ==========================================================================================================================
 func _ready():
 	Game_Manager.connect("pass_turn", self, "toggle_pass_turn_button")
 	$Character_Info.hide()
 	for child in $Actions.get_children():
 		child.set_action(null)
-		child.connect("action_pressed", Combat_Manager, "action_selected")
+		child.connect("action_pressed", self, "action_pressed")
 
 #Plays the game start animation with all portraits set up
 func game_start():
@@ -32,22 +33,37 @@ func game_start():
 	
 	$AnimationPlayer.play("game_start")
 
-func toggle_pass_turn_button(isPlayersTurn):
-	$Pass.disabled = !isPlayersTurn
-
 func quit():
 	get_tree().quit()
+#Setup ==========================================================================================================================
 
+#Turn passing ===================================================================================================================
 func pass_turn():
 	Game_Manager.pass_turn()
 
+func toggle_pass_turn_button(isPlayersTurn):
+	$Pass.disabled = !isPlayersTurn
+#Turn passing ===================================================================================================================
+
+#Character selection and actions ================================================================================================
+var selectedCharacter:Character = null
+
+func action_pressed(action):
+	if selectedCharacter != null:
+		Combat_Manager.action_selected(selectedCharacter, action)
+
 #Selects a character
 func select_character(character:Character):
-	var actions = Character_Manager.characterData[character.characterName]["Abilities"]
-	for child in $Actions.get_children():
-		if child.get_index() < actions.size():
-			child.set_action(actions[child.get_index()])
-		else:
+	selectedCharacter = character
+	if character.isPlayerOwned:
+		var actions = Character_Manager.characterData[character.characterName]["Abilities"]
+		for child in $Actions.get_children():
+			if child.get_index() < actions.size():
+				child.set_action(actions[child.get_index()])
+			else:
+				child.set_action(null)
+	else:
+		for child in $Actions.get_children():
 			child.set_action(null)
 	
 	$Character_Info.show()
@@ -56,7 +72,9 @@ func select_character(character:Character):
 
 #Clears character selection
 func unselect_character():
+	selectedCharacter = null
 	for child in $Actions.get_children():
 		child.set_action(null)
 	
 	$Character_Info.hide()
+#Character selection and actions ================================================================================================
